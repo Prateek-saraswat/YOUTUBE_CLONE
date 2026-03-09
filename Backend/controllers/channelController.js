def4f66af2879f778ef0e0bc657ed33ee7665990
Prateek Saraswat <saraswatprateek0@gmail.com>
2026-03-10 02:36:37 +0530

    Channel create controller done

--- Backend/controllers/channelController.js
+++ Backend/controllers/channelController.js
@@ -1,21 +1,42 @@
-import User from '../models/User.js'
-import Channel from '../models/Channel'
+import User from "../models/User.js";
+import Channel from "../models/Channel.js";
 
-export const createChannel = async (req , res)=> {
+export const createChannel = async (req, res) => {
+  try {
+    const { channelName, description, channelBanner } = req.body;
 
-    const user  = req.user
-    const {channelName ,  description , channelBanner } = req.body
-
-    if(!channelName || !description || !channelBanner ){
-        return 
+    if (!channelName) {
+      return res.status(400).json({ message: "Channel name is required" });
     }
 
-    const uniqueChannel = await Channel.findOne({channelName})
+    const uniqueChannel = await Channel.findOne({ channelName });
 
-    if(uniqueChannel){
-        return res.status(400).json({ message: "Channel name already taken" });
+    if (uniqueChannel) {
+      return res.status(400).json({ message: "Channel name already taken" });
     }
 
+    const user = await User.findById(req.user._id);
 
+    if (user.channel !== null) {
+      return res.status(400).json({ message: "You already have a channel" });
+    }
 
-}
+    const newChannel = await Channel.create({
+      channelName,
+      description,
+      channelBanner,
+      owner: req.user._id,
+    });
+
+    await User.findByIdAndUpdate(req.user._id, {
+      channel: newChannel._id,
+    });
+
+    res.status(201).json({
+      message: "Channel created successfully",
+      channel: newChannel,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Backend/models/Channel.js
+++ Backend/models/Channel.js
@@ -20,7 +20,6 @@ const channelSchema = new mongoose.Schema({
 
     },
     channelBanner:{
-
         type:String,
         default:""
 
